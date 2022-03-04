23e1e26d7811c352dec255a72191edb676a8803e
ayshasinara <97454953+ayshasinara@users.noreply.github.com>
2022-03-05 00:18:52 +0530

    Add files via upload

---
+++ my_test/main.js
@@ -0,0 +1,12 @@
+const add= require("./add");
+
+const sub= require("./subtract")
+const mul = require("./multiply");
+
+const div = require("./divide");
+var a=5;
+var b=3
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
